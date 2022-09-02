c8263c0e950d6e1f30aeadf78c5349e4bfa4d373
teamriders098 <111563785+teamriders098@users.noreply.github.com>
2022-09-02 10:32:43 +0530

    All commands added

--- setup2.sh
+++ setup2.sh
@@ -2,5 +2,6 @@ git clone https://github.com/Datalux/Osintgram.git
 cd Osintgram
 python3 -m venv venv
 source venv/bin/activate
+.\venv\Scripts\activate.ps1
 pip install -r requirements.txt
-Make setup
+make setup
