cf0a269047deff8bf64ce92d25a13d6c3c9f5e0d
teamriders098 <111563785+teamriders098@users.noreply.github.com>
2022-09-02 10:39:45 +0530

    Added pip & pip3 upgrading commands

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 python3 -m venv venv
 source venv/bin/activate
 .\venv\Scripts\activate.ps1
+pip install --upgrade pip
+pip install --upgrade pip3
+pip3 install --upgrade pip
+pip3 install --upgrade pip3
 pip install -r requirements.txt
 pip3 install -r requirements.txt
 make setup
